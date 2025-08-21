424b00bc5642a170bee57a90b0158d6a3b6c32f9
Mahbubul Hasan <mahbubul@kitegamesstudio.com>
2025-08-21 13:26:58 +0600

    Initial commit

---
+++ Followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Followers
+//
+//  Created by Mahbubul Hasan on 21/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FollowersTests/FollowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  FollowersTests.swift
+//  FollowersTests
+//
+//  Created by Mahbubul Hasan on 21/8/25.
+//
+
+import Testing
+@testable import Followers
+
+struct FollowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
